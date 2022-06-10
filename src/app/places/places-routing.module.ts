2b5fa2bf2a1a36dfa6bb7ee12fe963a3e5cd9b50
yingyingquek <quekyingying26@gmail.com>
2022-06-10 15:53:02 +0800

    routing set up for places

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,17 +5,71 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./discover/discover.module').then(
+                (m) => m.DiscoverPageModule
+              ),
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import(
+                './discover/place-detail/place-detail-routing.module'
+              ).then((m) => m.PlaceDetailPageRoutingModule),
+          },
+          {
+            path: '',
+            redirectTo: '/places/tabs/discover',
+            pathMatch: 'full',
+          },
+        ],
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./offers/offers.module').then((m) => m.OffersPageModule),
+          },
+          {
+            path: 'new',
+            loadChildren: () =>
+              import('./offers/new-offer/new-offer-routing.module').then(
+                (m) => m.NewOfferPageRoutingModule
+              ),
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () =>
+              import('./offers/edit-offer/edit-offer-routing.module').then(
+                (m) => m.EditOfferPageRoutingModule
+              ),
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import(
+                './offers/offer-bookings/offer-bookings-routing.module'
+              ).then((m) => m.OfferBookingsPageRoutingModule),
+          },
+        ],
+      },
+    ],
+  },
+  {
     path: '',
-    component: PlacesPage
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full',
   },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  }
 ];
 
 @NgModule({
